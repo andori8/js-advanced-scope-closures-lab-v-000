8b9ee7bf18813b4461f8089d7eeb5f161127196c
Ryan Do <falcsmike7@gmail.com>
2017-11-09 19:57:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function produceDrivingRange(blockRange) {
+  return function(startBlock, endBlock) {
+    let start = parseInt(startBlock)
+    let end = parseInt(endBlock)
+    let trip = Math.abs(start - end)
+    let range = blockRange - trip
+
+    if (range > 0) {
+      return `within range by ${range}`
+    } else {
+      return `${Math.abs(range)} blocks out of range`
+    }
+  }
+}
+
+function produceTipCalculator(percent) {
+  return function(fare) {
+    return fare * percent
+  }
+}
