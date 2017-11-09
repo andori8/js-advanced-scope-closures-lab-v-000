c4c49bad5f3e38304df762dcffb598091fff03d8
Ryan Do <falcsmike7@gmail.com>
2017-11-09 19:52:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function produceDrivingRange(blockRange) {
+  return function(startBlock, endBlock) {
+    let start = parseInt(startBlock)
+    let end = parseInt(endBlock)
+    let trip = Math.abs(start - end)
+    let range = blockRange - trip
+
+    if (range > 0) {
+      return `within range by ${range}`
+    } else {
+      return `${Math.abs(range)} out of range`
+    }
+  }
+}
